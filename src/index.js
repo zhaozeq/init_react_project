67329c85b7debcee7ace76cdcdbf4771f0f2eeb6
Zhao <zhaozeq@foxmail.com>
2018-07-19 19:16:26 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = '黑体文字';
+    element.innerHTML = 'Hello,World！';
     return element;
 }
 document.getElementById('root').appendChild(component());
